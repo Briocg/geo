9adfa111f58cd4a0a89d5fa502675156ec336bfb
Brio <157863318+Briocg@users.noreply.github.com>
2024-09-24 22:55:40 +0800

    Add files via upload

---
+++ Code/Geo.js
@@ -0,0 +1,13 @@
+/*const centerdot = document.getElementById('centerdot')
+const centercoords = centerdot.getBoundingClientRect()
+console.log(centercoords.x,centercoords.y)
+
+document.addEventListener('click', function (event) {
+    const coordinatesElement = document.getElementById('coordinates');
+    const x = event.clientX;
+    const y = event.clientY;
+    const distance = Math.sqrt((centercoords.x-x)**2+(centercoords.y-y)**2).toFixed(2)
+    coordinatesElement.textContent = `Clicked at coordinates: (${x}, ${y}). Distance from the actual location is ` + distance;
+    });
+*/
+
